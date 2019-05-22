2e4428ec9e8848f3472fa4c32d46d4a724a3f094
tosine6 <adetosine6@gmail.com>
2019-05-22 17:13:29 +0100

    added new feature to check remark

--- GradingSystem.java
+++ GradingSystem.java
@@ -7,28 +7,55 @@ public class GradingSystem {
         if (score >= 70 && score <= 100){         
             return 'A';
         }
-        else if (score >=60 && score <= 69){
+        else if (score >=60){
             return 'B';
         }
-        else if (score >= 50 && score <= 59){
+        else if (score >= 50){
             return 'C';
         }
-        else if (score >= 45 && score <= 49){
+        else if (score >= 45){
             return 'D';
         }
-        else if (score >= 40 && score <= 44){
+        else if (score >= 40){
             return 'E';
         }
         else {
             return 'F';
         }
     }
+
+    public String Remark(char grade){
+        String remark;
+
+        switch (grade){
+            case 'A':
+            remark = "excellent";
+            break;
+
+            case 'B':
+            remark = "verygood";
+            break;
+
+            case 'C':
+            remark = "good";
+            break;
+
+            case 'D':
+            remark = "poor";
+            break;
+
+            default:
+            remark = "Advice to withdraw";
+        }
+    return remark;
+    }
     public static void main (String[]args){
 
         GradingSystem saliu = new GradingSystem();
+        GradingSystem comment = new GradingSystem();
 
         // print
 
-        System.out.print(saliu.Grade(30));
+        System.out.printf("Your grade is %s" + " and you're %s", saliu.Grade(30), comment.Remark('F'));
     }
 }
